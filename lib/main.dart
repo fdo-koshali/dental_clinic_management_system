b8d91939f91c08cba1c60400da09212c312b3037
fdo-koshali <fdokoshali@gmail.com>
2025-03-23 12:23:32 +0530

    Update: Project file structure reorganization

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:dental_clinic_management_system/splash_screen.dart';
-import 'package:dental_clinic_management_system/welcome_screen.dart';
-import 'package:dental_clinic_management_system/internet_connection_error.dart';
-import 'package:dental_clinic_management_system/home_screen.dart';
-import 'package:dental_clinic_management_system/appointment_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -18,17 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Dentos',
       debugShowCheckedModeBanner: false,
-      title: 'Dental Clinic Management System',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const SplashScreen(), // Starts with SplashScreen
-        '/welcome': (context) => const WelcomeScreen(),
-        '/noInternet': (context) => const InternetConnectionError(),
-        '/home': (context) => HomeScreen(),
-        '/appointments': (context) => AppointmentScreen(),
-      },
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
     );
   }
 }
